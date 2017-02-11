75cf8d4ffbc1e0b2ee4de5bbd392b4202523361f
Adam Reszka <adam.reszka85@gmail.com>
2017-02-11 15:15:02 +0100

    add script for hamburger

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ $(document).ready(initAll);
 function initAll() {
 	$("a").on('click', function(event) {
 		event.preventDefault();
-	})
+	});
+	$("#hamburger").on('click', function(event){
+		event.preventDefault();
+		alert('udao sie');
+	});
 }
